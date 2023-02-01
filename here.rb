7120d322777e82c7e43a684bde5ef6097917e1a9
Winga Isaac Ronney <110449714+1Ronney@users.noreply.github.com>
2023-02-01 18:25:24 +0300

    Create here.rb

---
+++ here.rb
@@ -0,0 +1,3 @@
+#!/usr/bin/env ruby
+# this the plan to get my cows and sheeps ready by the end of the first quater of the year indipendent of what will happen
+puts ARGV[0].scan(/"cows.sheeps,and goats, but.*?\goats&sheps"\).join
