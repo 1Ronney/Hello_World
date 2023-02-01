ee1b8fc12396dda68601a5c5d4485fdf003974bd
Winga Isaac Ronney <110449714+1Ronney@users.noreply.github.com>
2023-02-01 18:21:49 +0300

    Create Todolst.c

---
+++ Todolst.c
@@ -0,0 +1,18 @@
+#include "MyTodoList.h"
+#this is my todolist in the next two months.
+int main()
+{
+  int goat, sheep
+  goat = 1, sheep = 1
+  for ( goat = 1; goat >= 20)
+  {
+    printf(%d, no of goats in 20 years\n);
+  }
+  else
+    printf(%d, no of shep in 20 years\n);
+  return (0):
+}
+    
+int price of goat = 2500 =! 4000.
+  & price of sheep = 1500 =! 300.
+  plans for March & April weeks. 
