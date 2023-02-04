f3809384c8bef166e5e1f1a25c755c3f71701905
Winga Isaac Ronney <110449714+1Ronney@users.noreply.github.com>
2023-02-04 15:10:25 +0300

    Create Medchem.rb

---
+++ Medchem.rb
@@ -0,0 +1,5 @@
+#!/usr/bin/env ruby
+def include(Medical Chemistry Topics)
+puts "medical #{Chemistry Topics}!"
+done:
+  here is the link: https://semmelweis.hu/admission/process/entrance-exam-medicine-dentistry-and-pharmaceutical-sciences/
