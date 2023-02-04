91c44e7284798bdda5ecbe02968fd447a53af604
Winga Isaac Ronney <110449714+1Ronney@users.noreply.github.com>
2023-02-04 15:05:55 +0300

    Create topic1.py

---
+++ topic1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+print(f"{"Evolution(Chemical & Biological Aspects of evolution)"}")
+while do :
+  topic two of : Chemical background in biology:
+      import structures:
+        atoms, chemical bonds, ionic compounds, and molecules.
+        for range in The Cell and its organization
+        {}    {}
+        {}    {}
+        import str.morphology_&_functions:
+          {[Prokaryotes(foocus on bacteria),
+            Eukaryotes ("focus on cilia & Flagella, cell wall, golgi aparatus
+                        ER, mitochodria, ribosomes,lysosomes, centrioles focus :
+                        here in the below link: 
+                        import module.website:"https://semmelweis.hu/admission/process/entrance-exam-medicine-dentistry-and-pharmaceutical-sciences/")]}
