073994c720d0fc7c28df0c0226d214c8a6fdc723
Winga Isaac Ronney <110449714+1Ronney@users.noreply.github.com>
2023-02-07 07:47:02 +0300

    Create biochem.py

---
+++ biochem.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+from modules import fondation.biochemistry
+import sy. features_to_distinguish_living_organisms
+print("f"(reasons are:)""):
+  while ; 
+  
+  1. organisms have higher chemical complexities: 
+    "meaning every thousad molecules make up cells internal structure and every organell with specific chemical subunits"
+  2. Orgnaisma have a systems for energy extraction, tranformation, and.consumption:
+    therefore: :
+        : "enables them build,maintain and perform chemical actions,osmotic & electrical work : :
+          however: the in non-living organisms, the life sequence tend to decay in a more disordersed state to enhance equilibrium in the sorounding.
+  3. Capacity for:
+   "pricise Self-replication & Assembly: :"
+  #high and low level organisms,for. bacteria_cells placed in sterile nutrient mediums give rise to billion identical cells.
+ # similarly the bactriums have identical copies of the parents.
+  4. Sensory and response : organism have a costant adjustment system to their environment.
+  5. def functions:
+    living organisms.have. componetns with specific functions and that controls the interraction of these componets.
+    #both microscopic and structures with intracellular with indivudual chemical componets interact internally
+    this is significant for both animals and plants. 
+    do:
+      {"(interplays between chemical components of a living orgnaisms are dynamic:
+       and the changes in one component activate and cordinate another process thus a system of 
+       cordinated programmes)"}
+  6. orgarnisms also have evolutionary changes:
+       "new environments elicits new adaptive meacsures:" or
+       "new characteristics"
+       #this is the backround analysis of biochemistry introduction
+       """
+       comments are as much important as notes themselves
+       """
+       done:
+       
